13bb982de7c376008f9cc06a550005a7589b4420
endzikri <endzikri@gmail.com>
2019-08-25 16:09:29 +0200

    Kretanje

--- main.c
+++ main.c
@@ -2,14 +2,30 @@
 #include <math.h>
 #include <GL/glut.h>
 
+#define timer_id1 0
+
 //callback fje
 static void on_keyboard(unsigned char key, int x, int y);
+static void on_release(unsigned char key,int x,int y);
 static void on_reshape(int width, int height);
 static void on_display();
 static int glavni_tajmer;
 
+double x_kocke = 3;
+double y_kocke = 1;
+double z_kocke = 0;
+
+double x1_putanje = 1;
+double x2_putanje = 3;
+double x3_putanje = 5;
+double y_putanje = 0.25;
+double z_putanje = 25;
+
 void kocka();
 void putanja();
+void igra(int value);
+
+static int kretanje[] = {0,0};
 
 int main(int argc, char **argv){
     glutInit(&argc, argv);
@@ -22,8 +38,9 @@ int main(int argc, char **argv){
     glutKeyboardFunc(on_keyboard);
     glutReshapeFunc(on_reshape);
     glutDisplayFunc(on_display);
+    glutKeyboardUpFunc(on_release);
 
-    timer_active = 0;
+    glavni_tajmer = 0;
 
     glClearColor(1, 1, 1, 0);
     glEnable(GL_DEPTH_TEST);
@@ -38,7 +55,25 @@ static void on_keyboard(unsigned char key, int x, int y){
     case 27:
         exit(0);
         break;
+    case 'p':
+    case 'P':
+            if(!glavni_tajmer){
+                    glutTimerFunc(30, igra ,timer_id1);
+                    glavni_tajmer = 1;
+            };break;
+    case 'a':
+    case 'A':
+        kretanje[0] = 1;
+        glutPostRedisplay();
+        break;
+    //skretanje u desno
+    case 'd':
+    case 'D':
+        kretanje[1] = 1;
+        glutPostRedisplay();
+        break;
     }
+    
 }
 
 static void on_reshape(int width, int height){
@@ -51,7 +86,8 @@ static void on_reshape(int width, int height){
     gluPerspective(60, (float) width / height, 1, 1500);
 }
 
-static void on_display(void){
+static void on_display(void)
+{
     
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -89,18 +125,69 @@ static void on_display(void){
     glutSwapBuffers();
 }
 
-void kocka(){
+static void on_release(unsigned char key,int x,int y)
+{
+    switch(key){
+        case 'a':
+        case 'A':
+            kretanje[0] -= 1;break;
+        case 'd':
+        case 'D': kretanje[1] -= 1;break;
+    }
+}
+
+void igra(int value)
+{
+    if(value)
+        return;
+
+    if(kretanje[0] && x_kocke < 5.5){
+        x_kocke += 0.2;
+    }
+    if(kretanje[1] && x_kocke > 0.5){
+        x_kocke -= 0.2;
+    }
+
+    z_putanje -= 0.2;
+
+    if(z_putanje <= -25) {
+        z_putanje = 25;
+    }
+
+    glutPostRedisplay();
+    if(glavni_tajmer) {
+        glutTimerFunc(30,igra,timer_id1);
+    }
+}
+
+void kocka()
+{
     glPushMatrix();
         glColor3f(0.6, 0, 0.4);
-        glTranslatef(3, 1, 0);
+        glTranslatef(x_kocke, y_kocke, z_kocke);
         glutSolidCube(0.5);
     glPopMatrix();
 }
-void putanja(){
+void putanja()
+{
     glPushMatrix();
-        glColor3f(0, 0.7, 0.2);
-        glTranslatef(3, 0.25, 25);
-        glScalef(6, 0.5, 50);
+        glColor3f(1, 0, 0);
+        glTranslatef(x1_putanje, y_putanje, z_putanje);
+        glScalef(2, 0.5, 50);
+        glutSolidCube(1);
+    glPopMatrix();
+
+    glPushMatrix();
+        glColor3f(0, 1, 0);
+        glTranslatef(x2_putanje, y_putanje, z_putanje);
+        glScalef(2, 0.5, 50);
+        glutSolidCube(1);
+    glPopMatrix();
+
+    glPushMatrix();
+        glColor3f(0, 0, 1);
+        glTranslatef(x3_putanje, y_putanje, z_putanje);
+        glScalef(2, 0.5, 50);
         glutSolidCube(1);
     glPopMatrix();
 }
